6e47b3713e11d0c6b024729fab71310a41927f49
Nikola Zorkic <zorkicdev@gmail.com>
2020-06-02 07:21:00 +0200

    added futureBuilder for displaying list of reminders

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:reminder_app/models/reminder.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -58,128 +57,148 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _buildReminderList(HomeViewModel model, BuildContext context) {
-    model.getReminders(ReminderState.upcoming);
-    List<Reminder> reminders = model.reminders;
-    print('length: ${reminders.length}');
-    if (reminders.isEmpty) {
-      return Center(
-        child: Text('No reminders.'),
-      );
-    }
-    return ListView.builder(
-      itemCount: reminders.length,
-      itemBuilder: (context, index) {
-        final reminder = reminders[index];
+    return FutureBuilder(
+      future: model.getReminders(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.none &&
+            snapshot.hasData == false) {
+          return Center(
+            child: Text('There are no reminders'),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("There was an error."),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              final reminder = snapshot.data[index];
 
-        return Padding(
-          padding: const EdgeInsets.only(
-            top: 10.0,
-            left: 10.0,
-            right: 10.0,
-            bottom: 5.0,
-          ),
-          child: Container(
-            color: Colors.black26,
-            child: ListTile(
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 6.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(reminder.text),
-                    GestureDetector(
-                      onTap: () => showModalBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Container(
-                            child: ListView(
-                              children: <Widget>[
-                                ListTile(
-                                  leading: Icon(Icons.check),
-                                  title: Text('Done'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.alarm),
-                                  title: Text('Postpone'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.create),
-                                  title: Text('Edit'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.content_copy),
-                                  title: Text('Copy'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.share),
-                                  title: Text('Share'),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.delete),
-                                  title: Text('Delete'),
-                                  onTap: () =>
-                                      model.deleteReminder(reminder.id),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                      child: Icon(
-                        Icons.more_vert,
-                        size: 18.0,
-                      ),
-                    )
-                  ],
+              return Padding(
+                padding: const EdgeInsets.only(
+                  top: 10.0,
+                  left: 10.0,
+                  right: 10.0,
+                  bottom: 5.0,
                 ),
-              ),
-              subtitle: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.access_time,
-                        size: 16.0,
+                child: Container(
+                  color: Colors.black26,
+                  child: ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(bottom: 6.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(reminder.text),
+                          GestureDetector(
+                            onTap: () => _buildBottomSheetModal(
+                                context, model, reminder),
+                            child: Icon(
+                              Icons.more_vert,
+                              size: 18.0,
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(
-                        width: 3.0,
-                      ),
-                      Text(reminder.time),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Icon(
-                        Icons.calendar_today,
-                        size: 16.0,
-                      ),
-                      SizedBox(
-                        width: 3.0,
-                      ),
-                      Text(reminder.date),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Icon(
-                        Icons.repeat,
-                        size: 16.0,
-                      ),
-                      SizedBox(
-                        width: 3.0,
-                      ),
-                      Text(reminder.repeat),
-                    ],
+                    ),
+                    subtitle: _buildTileSubtitle(reminder),
                   ),
-                  Icon(
-                    Icons.star,
-                    size: 18.0,
-                  )
-                ],
+                ),
+              );
+            },
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
+  }
+
+  _buildBottomSheetModal(context, model, reminder) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text('Done'),
               ),
-            ),
+              ListTile(
+                leading: Icon(Icons.alarm),
+                title: Text('Postpone'),
+              ),
+              ListTile(
+                leading: Icon(Icons.create),
+                title: Text('Edit'),
+              ),
+              ListTile(
+                leading: Icon(Icons.content_copy),
+                title: Text('Copy'),
+              ),
+              ListTile(
+                leading: Icon(Icons.share),
+                title: Text('Share'),
+              ),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text('Delete'),
+                onTap: () => model.deleteReminder(reminder.id),
+              ),
+            ],
           ),
         );
       },
+    );
+  }
+
+  Row _buildTileSubtitle(reminder) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Icon(
+              Icons.access_time,
+              size: 16.0,
+            ),
+            SizedBox(
+              width: 3.0,
+            ),
+            Text(reminder.time),
+            SizedBox(
+              width: 10.0,
+            ),
+            Icon(
+              Icons.calendar_today,
+              size: 16.0,
+            ),
+            SizedBox(
+              width: 3.0,
+            ),
+            Text(reminder.date),
+            SizedBox(
+              width: 10.0,
+            ),
+            Icon(
+              Icons.repeat,
+              size: 16.0,
+            ),
+            SizedBox(
+              width: 3.0,
+            ),
+            Text(reminder.repeat),
+          ],
+        ),
+        Icon(
+          Icons.star,
+          size: 18.0,
+        )
+      ],
     );
   }
 }
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -6,19 +6,16 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  List<Reminder> reminders = [];
-
   DatabaseService databaseService = locator<DatabaseService>();
   NotificationService notificationService = locator<NotificationService>();
 
-  void getReminders([ReminderState state]) async {
-    reminders = await databaseService.reminders();
+  Future<List<Reminder>> getReminders([ReminderState state]) {
+    return databaseService.reminders();
   }
 
   void deleteReminder(int id) async {
     await notificationService.deleteReminder(id);
     await databaseService.deleteReminder(id);
-    reminders.removeWhere((element) => element.id == id);
     notifyListeners();
     locator<NavigationService>().popRepeated(1);
   }
